f504baca9d2b544f5ad5951d4a1a05fa0dd5f951
Hossam <codingWorld5000@gmail.com>
2025-11-19 23:48:14 +0200

    learning about date

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // vars & cons
 const Lis = document.querySelectorAll("ul li");
 const selected = document.querySelector(".selected");
+const date = Date.now();
 
 // logic
 selected.style.backgroundColor = "green";
 selected.style.color = "white";
 
+
 // test
-console.log(selected.getElementsByClassName("num"));
+console.log(date);
