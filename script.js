02485ba4594a6ce1c208aa2e459a991054dcf01c
Hossam <codingWorld5000@gmail.com>
2025-11-28 05:50:36 +0200

    try to learn regex

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 const Lis = document.querySelectorAll("ul li");
 const selected = document.querySelector(".selected");
 const date = new Date(2025, 10, 21, 9, 0, 0, 0);
-
+const regex = new RegExp(/abc/);
+let str = "Hi, what is your abc";
 // logic
 selected.style.backgroundColor = "green";
 selected.style.color = "white";
 
 
 // test
-console.log(date.toLocaleString("en-UK"));
+console.log(str.match(regex));
