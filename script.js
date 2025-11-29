e039411b91ad03ef0d783648d6da5d6a082e36e9
Hossam <codingWorld5000@gmail.com>
2025-11-29 05:06:50 +0200

    update

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const Lis = document.querySelectorAll("ul li");
 const selected = document.querySelector(".selected");
 
 const regex = new RegExp(/\bfox\b/g);
-let str = "A quick fox profox foxation can run very fast";
+let str = `A quick fox profox foxation can run very fast
+so any fox can run slowly and foxy trifox can't catch it.
+`;
 // logic
 selected.style.backgroundColor = "green";
 selected.style.color = "white";
